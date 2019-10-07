a225f052aba634370cbdbc9544a9b28fb1776d06
Frederico Dargel <Frederico.Dargel@gmail.com>
2019-10-07 20:08:34 -0300

    Add files via upload

---
+++ definitions.h
@@ -0,0 +1,23 @@
+// Utility:
+#define CLEAR "\e[H\e[2J" // CLEARs screen
+#define RESET "\e[0m" // RESETs colors
+
+// Regular text color:
+#define BLK "\e[0;30m" // BLacK
+#define RED "\e[0;31m" // RED
+#define GRN "\e[0;32m" // GReeN
+#define YEL "\e[0;33m" // YELlow
+#define BLU "\e[0;34m" // BLUe
+#define MAG "\e[0;35m" // MAGenta
+#define CYN "\e[0;36m" // CYAn
+#define WHT "\e[0;37m" // WHiTe
+
+// Regular Bold text color:
+#define BBLK "\e[1;30m" // Bold BLacK
+#define BRED "\e[1;31m" // Bold RED
+#define BGRN "\e[1;32m" // Bold GReeN
+#define BYEL "\e[1;33m" // Bold YELlow
+#define BBLU "\e[1;34m" // Bold BLUe
+#define BMAG "\e[1;35m" // Bold MAGenta
+#define BCYN "\e[1;36m" // Bold CYAn
+#define BWHT "\e[1;37m" // Bold WHiTe
